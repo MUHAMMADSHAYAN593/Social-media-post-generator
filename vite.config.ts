4b54521d3518a7e3f24969b1eed7512d359e6feb
Muhammad shayan <muhammadshayanakhlaq@gmail.com>
2026-01-17 14:52:45 +0500

    Final Fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
+import {nitro} from 'nitro/vite'
 
 const config = defineConfig({
   plugins: [
@@ -15,6 +15,9 @@ const config = defineConfig({
     tailwindcss(),
     tanstackStart(),
     viteReact(),
+    nitro({
+      preset: 'vercel'
+    })
   ],
 })
 
